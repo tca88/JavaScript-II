d5e31454defb1ec1a8a05dd82ba30c517cd8b4ca
tca88 <trishnaamin@gmail.com>
2019-02-26 18:13:09 -0500

    Complete challenges within callbacks.js file.

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -24,32 +24,111 @@ const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
 
 */
 
+/////////////////////////////
+
+// function getLength(arr, cb) {
+//   // getLength passes the length of the array into the callback.
+// }
+
+//ANSWER
 
 function getLength(arr, cb) {
-  // getLength passes the length of the array into the callback.
+  return cb(arr.length)
 }
+
+function lengthCallback (arrLength) {
+  console.log(arrLength);
+}
+
+// Function invocation
+
+getLength(items, lengthCallback);
+
+/////////////////////////////
+
+// function last(arr, cb) {
+//   // last passes the last item of the array into the callback.
+// }
+
+//ANSWER
 
 function last(arr, cb) {
-  // last passes the last item of the array into the callback.
+  return cb(arr[arr.length-1]);
 }
+
+function lastNameCallback(lastItem) {
+  console.log(lastItem);
+}
+
+// Function invocation
+
+last(items, lastNameCallback);
+
+/////////////////////////////
+
+// function sumNums(x, y, cb) {
+//   // sumNums adds two numbers (x, y) and passes the result to the callback.
+// }
+
+//ANSWER
 
 function sumNums(x, y, cb) {
-  // sumNums adds two numbers (x, y) and passes the result to the callback.
+  return cb(x + y);
 }
+
+function sumCallback(sum) {
+  console.log(sum);
+}
+
+// Function invocation
+
+sumNums(2, 3, sumCallback);
+
+/////////////////////////////
+
+// function multiplyNums(x, y, cb) {
+//   // multiplyNums multiplies two numbers and passes the result to the callback.
+// }
+
+//ANSWER
 
 function multiplyNums(x, y, cb) {
-  // multiplyNums multiplies two numbers and passes the result to the callback.
+  return cb(x * y);
 }
 
-function contains(item, list, cb) {
-  // contains checks if an item is present inside of the given array/list.
-  // Pass true to the callback if it is, otherwise pass false.
+function multiplyCallback(multiplication) {
+  console.log(multiplication);
 }
+
+// Function invocation
+
+multiplyNums(4, 4, multiplyCallback);
+
+/////////////////////////////
+
+// function contains(item, list, cb) {
+//   // contains checks if an item is present inside of the given array/list.
+//   // Pass true to the callback if it is, otherwise pass false.
+// }
+
+//ANSWER
+
+function contains(item, list, cb) {
+  return cb(list.includes(item));
+}
+
+function containsItemCallback(result) {
+  console.log(result);
+}
+
+contains("Pencil", items, containsItemCallback);
+
+/////////////////////////////
 
 /* STRETCH PROBLEM */
 
-function removeDuplicates(array, cb) {
-  // removeDuplicates removes all duplicate values from the given array.
-  // Pass the duplicate free array to the callback function.
-  // Do not mutate the original array.
-}
+// function removeDuplicates(array, cb) {
+//   // removeDuplicates removes all duplicate values from the given array.
+//   // Pass the duplicate free array to the callback function.
+//   // Do not mutate the original array.
+// }
